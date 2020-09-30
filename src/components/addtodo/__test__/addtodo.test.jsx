2b5a3a712706e14cdabcadc106c6fd37a8b47876
subhasmita7592 <40867418+subhasmita7592@users.noreply.github.com>
2020-09-30 09:23:27 +0530

    after test

--- src/components/addtodo/__test__/addtodo.test.jsx
+++ src/components/addtodo/__test__/addtodo.test.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { shallow,mount } from 'enzyme';
-import AddTodo from '../Addtodo'
+import React from "react";
+import { shallow, mount } from "enzyme";
+import AddTodo from "../Addtodo";
 
-describe('todoform component',()=>{
-    beforeEach(() => {
-         wrapper = shallow(<AddTodo/>);
-    });
+describe("todoform component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<AddTodo />);
+  });
 
-    it('render click event', () => {
-            wrapper.find('#primary').simulate('click');
+  it("render click event", () => {
+    wrapper.find("#primary").simulate("click");
 
-        expect(wrapper.find("#primary").text).toBe(true);
-    })
-
-
-})
+    expect(wrapper.find("#primary").text()).toBe("Add");
+  });
+});
--- src/components/list/__test__/todolist.test.jsx
+++ src/components/list/__test__/todolist.test.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { shallow,mount } from 'enzyme';
-import TodoList from '../TodoList'
+import React from "react";
+import { shallow, mount } from "enzyme";
+import TodoList from "../TodoList";
 
-describe('layout component',()=>{
-    beforeEach(() => {
-         wrapper = shallow(<TodoList/>);
-    });
+const todos = [{ text: "hello", checked: false }];
+describe("layout component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<TodoList items={todos} />);
+  });
 
-    it('renders list', () => {
-        
-        expect(wrapper.find("Todo").length).toBe(todos.length);
-    })
+  it("renders list", () => {
+    expect(wrapper.find("TodoListItem").length).toBe(todos.length);
+  });
 
-    it('renders list', () => {
-        expect(wrapper.find("TodoListItem").at(0).key()).toBe(true);
-    })
-
-
-})
+  it("renders list", () => {
+    expect(wrapper.find("TodoListItem").at(0).key()).toBe("0");
+  });
+});
